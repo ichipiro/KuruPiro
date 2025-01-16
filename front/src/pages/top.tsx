8bee1f13ac2c940e8775c083c71fbaf5d8147369
Huyu2239 <huyu2239@gmail.com>
2025-01-17 00:07:05 +0900

    fix: レスポンス修正に伴うフロント修正

--- front/src/pages/top.tsx
+++ front/src/pages/top.tsx
@@ -50,8 +50,8 @@ function NextBusesList() {
               <div key={index} className="NextBusesListCell">
                 <p className="BusName">{element.trip_id}<br />{element.trip_dest}</p>
                 <p className="StaticTime">{element.arrival_time}</p>
-                <p className="RemainingMinutes">{element.current_locate}</p>
-                <p className="DelayMinutes">{element.delay === "-1分遅れ" ? ("") : (element.delay)}</p>
+                <p className="RemainingMinutes">{element.current_locate === "" ? ("出発待ち") : (element.current_locate)}</p>
+                <p className="DelayMinutes">{element.delay/* === "-1分遅れ" ? ("") : (element.delay)*/}</p>
               </div>
             ))}
           </div>
@@ -62,7 +62,7 @@ function NextBusesList() {
                 <p className="BusName">{element.trip_id}<br />{element.trip_dest}</p>
                 <p className="StaticTime">{element.arrival_time}</p>
                 <p className="RemainingMinutes">{element.current_locate}</p>
-                <p className="DelayMinutes">{element.delay === "-1分遅れ" ? ("") : (element.delay)}</p>
+                { <p className="DelayMinutes">{element.delay/* === "-1分遅れ" ? ("") : (element.delay)*/}</p>}
               </div>
             ))}
           </div>
