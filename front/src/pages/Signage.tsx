a388c2c5102d07b5892a06f14d77649216be6fcf
Takuma Tateishi <takutate.0427@gmail.com>
2025-12-03 09:35:59 +0900

    [Fix] 10分以内におすすめ条件を変更

--- front/src/pages/Signage.tsx
+++ front/src/pages/Signage.tsx
@@ -23,7 +23,7 @@ export default function Signage() {
   // おすすめロジック:
   // - 沼田1便目が市大に来る → 市大をおすすめ（沼田はおすすめなし）
   // - 沼田1便目が市大に来ない場合:
-  //   - 沼田まで10分以上 かつ 市大1便目より早い → 沼田をおすすめ
+  //   - 沼田まで10分以内 かつ 市大1便目より早い → 沼田をおすすめ
   //   - それ以外 → 市大をおすすめ
   const { numaRecommendedIndex, piroRecommendedIndex } = (() => {
     if (piroData.length === 0 || numaData.length === 0) {
@@ -43,10 +43,10 @@ export default function Signage() {
       return { numaRecommendedIndex: -1, piroRecommendedIndex: 0 }
     } else {
       // 沼田が市大に来ない場合
-      // 沼田まで10分以上(600秒) かつ 市大1便目より早いなら沼田をおすすめ
-      const hasEnoughTime = numaBus.remainingSeconds >= 600
+      // 沼田まで10分以内(600秒) かつ 市大1便目より早いなら沼田をおすすめ
+      const isWithin10Min = numaBus.remainingSeconds <= 600
       const isFasterThanPiro = numaBus.remainingSeconds < piroBus.remainingSeconds
-      if (hasEnoughTime && isFasterThanPiro) {
+      if (isWithin10Min && isFasterThanPiro) {
         return { numaRecommendedIndex: 0, piroRecommendedIndex: -1 }
       } else {
         // そうでなければ市大をおすすめ
