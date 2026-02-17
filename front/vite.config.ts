75c0590c41dbad09d9303cc9007004d1c355fa2e
Takuma Tateishi <takutate.0427@gmail.com>
2026-02-17 15:57:42 +0900

    [Add] PWA対応

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,9 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // サービスワーカーを自動的に更新する設定
+      manifest: {
+        name: "くるぴろ",
+        short_name: "くるぴろ",
+        description: "広島市立大学周辺のバス時刻を表示するアプリ",
+        theme_color: "#005394",
+        background_color: "#005394",
+        display: "standalone",
+        orientation: "portrait",
+        start_url: "/",
+        icons: [
+          {
+            src: "/logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       '/api': {
